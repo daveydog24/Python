ae357a5a88014837de668b1fd4b424293940e782
David Wukelic <david.wukelic@gmail.com>
2018-10-28 18:39:08 -0700

    added basic fundamental practice

---
+++ python_fundamentals/for_loop_basic1.py
@@ -0,0 +1,41 @@
+# 1 - Basic
+# for x in range(0, 151):
+#     print(x)
+
+# 2 - Multiples of Five
+# for x in range(5, 1000001, 5):
+#     print(x)
+
+# 3 - Counting, the Dojo Way
+# for x in range(1, 101):
+#     if (x % 5 == 0):
+#         print("Coding")
+#         if (x % 10 == 0):
+#             print("Dojo")
+
+# 4 - Whoa. That Sucker's Huge
+# total = 0
+# for x in range(0, 500000):
+#     if (x % 2 == 1):
+#         total += x
+# print(total)
+
+# 5 - Countdown by Fours
+# starting_number = 2018
+# while starting_number > 0:
+#     print(starting_number)
+#     starting_number -= 4
+
+# 6 - Flexible Countdown
+# def Flexible_Countdown(low, high, multiple):
+#     for x in range(low, high + 1):
+#         if(x % multiple == 0):
+#             print(x)    
+
+# Flexible_Countdown(2, 9, 3)
+
+new_list = [3,5,1,2]
+
+for i in range(new_list):
+    print(i)
+    
