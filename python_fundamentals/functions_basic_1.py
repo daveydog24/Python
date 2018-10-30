7917bd848939b84dca0bfd3d5d2013a3e557392c
David Wukelic <david.wukelic@gmail.com>
2018-10-29 19:34:30 -0700

    testing f-strings

--- python_fundamentals/functions_basic_1.py
+++ python_fundamentals/functions_basic_1.py
@@ -1,8 +1,17 @@
-1. 5
-2. 10
-3. 5
-4. 5
-5. 5, null  
-6. Error
-7. "25"
-8. 
+# 1. 5
+# 2. 10
+# 3. 5
+# 4. 5
+# 5. 5, null  
+# 6. Error
+# 7. "25"
+# 8. 
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+
+
+
