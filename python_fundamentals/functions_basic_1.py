f1bfdfd83c5ba756b8691ad3952a3714ec9e9c7f
David Wukelic <david.wukelic@gmail.com>
2018-10-29 21:02:50 -0700

    testing python3 new features

--- python_fundamentals/functions_basic_1.py
+++ python_fundamentals/functions_basic_1.py
@@ -10,8 +10,22 @@
 first_name = "Zen"
 last_name = "Coder"
 age = 27
-print(f"My name is {first_name} {last_name} and I am {age} years old.")
+x = (f"My name is {first_name} {last_name} and I am {age} years old.")
 
+check_list = [x, 23, "Hi Kim", 1231, "Friend", 44, "This is a test"]
+
+for x in check_list:
+    if isinstance(x, str):
+        x = x.split()
+        for i in x:
+            print(f"Single Word: {i}")
+    elif isinstance(x, int):
+        print (f"Number: {x}")
+
+# x = x.split()
+# for i in x:
+#     if i.isalnum():
+#         print(i)
 
 
 
