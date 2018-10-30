3077bb8c69323ecbe7d42923f70a84019a3d8abf
David Wukelic <david.wukelic@gmail.com>
2018-10-29 21:06:44 -0700

    adding function capability

--- python_fundamentals/functions_basic_1.py
+++ python_fundamentals/functions_basic_1.py
@@ -14,18 +14,21 @@ x = (f"My name is {first_name} {last_name} and I am {age} years old.")
 
 check_list = [x, 23, "Hi Kim", 1231, "Friend", 44, "This is a test"]
 
-for x in check_list:
-    if isinstance(x, str):
-        x = x.split()
-        for i in x:
-            print(f"Single Word: {i}")
-    elif isinstance(x, int):
-        print (f"Number: {x}")
 
-# x = x.split()
-# for i in x:
-#     if i.isalnum():
-#         print(i)
+def print_out_everything_by_itself(check_list):
+    for x in check_list:
+        if isinstance(x, str):
+            x = x.split()
+            for i in x:
+                print(f"Single Word: {i}")
+        elif isinstance(x, int):
+            print (f"Number: {x}")
+        elif isinstance(x, list):
+            print_out_everything_by_itself(x)
+
+print_out_everything_by_itself(check_list)
+            
+        
 
 
 
