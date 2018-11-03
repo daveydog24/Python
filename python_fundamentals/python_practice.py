a14b37eac82bac3ed5632821756b600004191873
David Wukelic <david.wukelic@gmail.com>
2018-11-03 01:44:33 -0700

    added callable function

---
+++ python_fundamentals/python_practice.py
@@ -0,0 +1,11 @@
+# This is a file to test python functions and techniques.
+
+def ASCII(str1, str2, total):
+    one = list(str1)
+    two = list(str2)
+    total = total
+    count = 0
+        
+str1 = 'boat'
+str2 = 'coat'
+print ASCII(str1, str2, 0)
