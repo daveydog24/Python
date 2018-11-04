3fc450bb81da854fc2a7a24e322d939bb902f46c
David Wukelic <david.wukelic@gmail.com>
2018-11-04 14:32:03 -0800

    created function to loop through key, value, size and print all information of dictionaries

--- python_fundamentals/functions_2.py
+++ python_fundamentals/functions_2.py
@@ -56,7 +56,7 @@ print(students)
 
 
 **************************************** SECTION 2 ***********************************************
-'''
+
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
@@ -64,11 +64,22 @@ students = [
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
 ]
 
-def iterateDictionary1(students):
+def iterateDictionary(students):
     for student in students:
         first_name = student["first_name"] 
         last_name = student["last_name"]
         print(f"first_name - {first_name}, last_name - {last_name}")
+
+iterateDictionary(students)
+
+**************************************** SECTION 3 ***********************************************
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
 
 def iterateDictionary2(word, students):
     for student in students:
@@ -80,15 +91,20 @@ def iterateDictionary2(word, students):
 
 iterateDictionary2('last_name', students)
     
-    
+'''
 
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}    
 
+def print_dictionary(array):
+    for x in array:
+        name = x.upper()
+        count = len(array[x]);
+        # python2 version print(str(count) + " " + str(name))
+        print(f"{count} {name}")
+        for value in array[x]:
+            print(value)
 
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 20} ]
-
-# sports_directory["basketball"] = "david"
-# print(sports_directory)
+print_dictionary(dojo)
