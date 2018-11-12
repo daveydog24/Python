c62bdd5974de7cc21bee6e39e94f960d5fbece44
David Wukelic <david.wukelic@gmail.com>
2018-11-11 16:09:34 -0800

    practice python closures

--- python_fundamentals/practice_python.py
+++ python_fundamentals/practice_python.py
@@ -95,7 +95,6 @@ stacks = 55
 print('Coding Dojo' if stacks >= 3 else 'You are not Coding Dojo!')
 # other language ternary operators
 print('Coding Dojo' ? stacks >= 3 : 'You are not Coding Dojo!')
-'''
 
 x = lambda num: num ** 2
 print(x(5))
@@ -121,3 +120,29 @@ def cubed(num):
 # this is a perfect example while anonymous functions with lambda are great
 print(list(map(cubed, my_arr)))
 print(list(map(lambda num: num ** 3, my_arr)))
+
+# map(), reduce(), sort(), filter() are all great functions where lambda implenmentaion comes into play
+'''
+# import logging
+# logging.basicConfig(filename = 'example.log', level=logging.INFO)
+
+def logger(func):
+    def log_func(*args):
+        # logging.info('Running "{}" with arguments {}'.format(func.__name__,args))
+        print(func(*args))
+    return log_func
+
+def add(x,y):
+    return x + y 
+
+def sub(x,y):
+    return x - y
+
+add_logger = logger(add)
+sub_logger = logger(sub)
+
+add_logger(3,3)
+add_logger(4,5)
+
+sub_logger(10,5)
+sub_logger(20, 10)
