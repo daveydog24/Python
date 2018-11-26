14daee0ca7d7d3164cb39c28691bff360c07060e
David Wukelic <david.wukelic@gmail.com>
2018-11-25 22:00:00 -0800

    attempting to add picture functionality

--- python_fundamentals/Tic_Tac_Toe/static/js/handleGame.js
+++ python_fundamentals/Tic_Tac_Toe/static/js/handleGame.js
@@ -1,3 +1,22 @@
+let counter = 1
+
+function takeTurn(){
+    counter++;
+    console.log("The turn is now at:", counter)
+}
+
 function changeSquare(num){
     console.log(num)
+    
+    if (counter % 2 == 1){
+        itemContent = document.getElementById(num).innerHTML(
+            `<img class="x" src="https://upload.wikimedia.org/wikipedia/commons/7/77/Letter_x.svg" alt="x">`
+        )
+    }
+    else{
+        itemContent = document.getElementById(num).innerHTML(
+            `<img class="o" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBd4LBnt4XNgIlH3phAUY6iAnOPem_WJsrAeSo8v7ONK00Xoeiqg" alt="o">`
+        )
+    }
+    takeTurn()
 }
