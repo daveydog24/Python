3e3bb0265712861666e702eccbacb60e82f0705d
David Wukelic <david.wukelic@gmail.com>
2018-11-26 22:05:56 -0800

    completed changeSquare function

--- python_fundamentals/Tic_Tac_Toe/static/js/handleGame.js
+++ python_fundamentals/Tic_Tac_Toe/static/js/handleGame.js
@@ -8,23 +8,15 @@ function takeTurn(){
 function changeSquare(num){
     num = "img" + num
     console.log(num)
-    document.getElementById(num).src = "https://upload.wikimedia.org/wikipedia/commons/7/77/Letter_x.svg";
-    document.getElementById(num).style.display = "block";
-    document.getElementById(num).className = "x";
     
-    // if (counter % 2 == 1){
-    //     document.getElementById(num).src = "<img class='x' src='https://upload.wikimedia.org/wikipedia/commons/7/77/Letter_x.svg' alt='x'>";
-    //     document.getElementById(num).style.display = "block";
-    // }
-    // else{
-    //     document.getElementById(num).innerHTML(
-    //         "<img class='x' src='https://upload.wikimedia.org/wikipedia/commons/7/77/Letter_x.svg' alt='x'>"
-    //     )
-    // }
-    // else{
-    //     document.getElementById(num).innerHTML(
-    //         `<img class="o" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBd4LBnt4XNgIlH3phAUY6iAnOPem_WJsrAeSo8v7ONK00Xoeiqg" alt="o">`
-    //     )
-    // }
+    if (counter % 2 == 1){
+        document.getElementById(num).src = "https://upload.wikimedia.org/wikipedia/commons/7/77/Letter_x.svg";
+        document.getElementById(num).style.display = "block";
+        document.getElementById(num).className = "x";   
+    } else {
+        document.getElementById(num).src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBd4LBnt4XNgIlH3phAUY6iAnOPem_WJsrAeSo8v7ONK00Xoeiqg";
+        document.getElementById(num).style.display = "block";
+        document.getElementById(num).className = "o";
+    }
     takeTurn()
 }
